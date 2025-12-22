1a2d355bcec2176b8da098b1d78523b5e5a86411
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-12-21 22:54:57 -0800

    Created newHabit component for habit creation step.

---
+++ src/components/newHabit.jsx
@@ -0,0 +1,34 @@
+
+
+export default function NewHabit() {
+    return (
+      <>
+        <section className="new-habit border-2 border-teal-600 p-4 rounded-lg bg-[220 16% 14%]  flex flex-col gap-4 relative text-start">
+          <div className="count w-5 absolute h-5 top-0 left-0 bg-teal-500 p-5 flex justify-center items-center rounded-xl shadow-2xl shadow-teal-300">
+            1
+          </div>
+          <h2 className="mt-10">Start New Habit Creation</h2>
+          <p className="welcome-board w-full rounded">
+            Welcome to your Habit creation wizard.
+          </p>
+          <article className="first-card-title">
+            <div className="article-header flex gap-4">
+              <div className="logo">HC</div>
+              <div className="info">
+                <h3>Launchpad</h3>
+                <p>Multi-step configuration</p>
+              </div>
+            </div>
+            <p>Create habits that stick to our giant habit control</p>
+          </article>
+          <div className="buttons">
+            <button>
+              {/* <span>icon</span> */}
+              Start habit creation
+            </button>
+            <button>Cancel</button>
+          </div>
+        </section>
+      </>
+    );
+}
