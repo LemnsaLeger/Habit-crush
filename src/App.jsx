daf389fd85110d356954cb9657aec3767e342b5b
LemnsaLeger <lemnsadigitalservices@gmail.com>
2026-01-06 13:04:38 -0800

    Modify App.jsx, Add Router for different pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
 import Header from './components/header';
-import './App.css'
+import './App.css';
 import CreationWizard from './pages/creationWizard';
+import SettingsPage from './components/settingsPage/settings';
+import DashboardPrime from "./pages/dashboard";
+
 
 function App() {
-
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main>
-        <CreationWizard />
-      </main> 
-    </>
+        <Routes>
+          <Route path="/" element={ <DashboardPrime/>  }/>
+          <Route path="create-habit" element={<CreationWizard/>}/>
+          <Route path="settings" element={<SettingsPage/>}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
