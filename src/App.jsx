c25325546a9488011d9c40f31afc1a11a6717e91
LemnsaLeger <lemnsadigitalservices@gmail.com>
2026-01-07 16:00:21 -0800

    Modify, App.jsx, Add Theme switch Handler logic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { useEffect, useState } from "react";
 
-import Header from './components/header';
+import Header from './components/header.jsx';
 import './App.css';
-import CreationWizard from './pages/creationWizard';
-import SettingsPage from './components/settingsPage/settings';
-import DashboardPrime from "./pages/dashboard";
+import CreationWizard from './pages/creationWizard.jsx';
+import SettingsPage from './components/settingsPage/settings.jsx';
+import DashboardPrime from "./pages/dashboard.jsx";
+import ThemeToggler from "./components/themeToggler.jsx";
 
 
 function App() {
+
+  const [theme, setTheme] = useState("teal");
+
+  useEffect(() => {
+    const root = document.documentElement;
+
+    if(theme === "purple") {
+      root.classList.add("theme-purple");
+    } else {
+      root.classList.remove("theme-purple");
+    }
+  }, [theme]);
+
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header onThemeChange={setTheme} />
       <main>
         <Routes>
           <Route path="/" element={ <DashboardPrime/>  }/>
